a3bc314944b9dbf950b3281b4f61f7136c7b4643
MuravlovaSvetlana <ushakovasvetlana94kh@gmail.com>
2021-04-09 19:40:00 +0300

    continue interceptor

--- src/app/interceptors/headers-interceptor.ts
+++ src/app/interceptors/headers-interceptor.ts
@@ -1,2 +1,10 @@
-export interface HeadersInterceptor {
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export interface HeadersInterceptor implements HttpInterceptor {
+
 }
