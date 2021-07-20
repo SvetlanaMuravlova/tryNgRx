13ddbbb427d93c0c34386fd800296b65f83453af
svetlanaMuravlova <svetlana.muravlova@valor-software.com>
2021-07-20 13:27:09 +0300

    test

--- src/app/modules/favourite-cities/components/general/general.component.ts
+++ src/app/modules/favourite-cities/components/general/general.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 // import { IweatherState } from '@generalStore/weather.reducer';
 import { IFavCitiesState } from '@fav-cities/favourite-cities.reducer';
-import { addKey } from '@fav-cities/favourite-cities.actions'
+import { addKey } from '@fav-cities/favourite-cities.actions';
 import { currentStateSelector } from '@fav-cities/favourite-cities.selectors';
 
 // import {GetConditionsByKey, GetLocationByKey } from '@generalStore/weather.actions'
@@ -18,14 +18,14 @@ import { ICondition } from '@interfaces/interfaces';
   styleUrls: ['./general.component.scss']
 })
 export class GeneralComponent implements OnInit {
-  type = 'favourite'
+  type = 'favourite';
   objectKeys = Object.keys;
   // conditions$: Observable<IFavCitiesState> = this.store$.pipe(select(weatherSelector))
   // list$: Observable<ICondition[]> = this.store$.pipe(select(GWConditionsListSelector))
   // generalLocations$: Observable<any> = this.store$.pipe(select(GWLocationsSelector))
   currentState$: Observable<IFavCitiesState> = this.store$.pipe(select(currentStateSelector))
   state: IFavCitiesState;
-  list : ICondition[];
+  list: ICondition[];
   generalLocations: any;
   constructor(
     private store$: Store<IFavCitiesState>,
@@ -34,36 +34,36 @@ export class GeneralComponent implements OnInit {
   ) {
     this.currentState$.subscribe(res => {
       this.state = res;
-    })
+    });
   }
 
   ngOnInit(): void {
-    this.getConditions();
+    // this.getConditions();
   }
 
-  getConditions() {
-    // let keysObserv = [];
-    // console.log(this.state.keys[0]);
-    this.state.keys.forEach(key => {
-      // keysObserv.push(this.apiService.getConditionBykey(key));
-      // this.store$.dispatch(GetLocationByKey({key}))
-      // this.store$.dispatch(GetConditionsByKey({key}))
-    })
-
-      // this.store$.dispatch(GetLocationByKey({key: this.state.keys[0]}))
-      // this.store$.dispatch(GetConditionsByKey({key: this.state.keys[0]}))
-
-    // this.store$.dispatch(GetConditionsByKey({key: this.state.keys[0]}))
-  }
+  // getConditions() {
+  //   // let keysObserv = [];
+  //   // console.log(this.state.keys[0]);
+  //   this.state.keys.forEach(key => {
+  //     // keysObserv.push(this.apiService.getConditionBykey(key));
+  //     // this.store$.dispatch(GetLocationByKey({key}))
+  //     // this.store$.dispatch(GetConditionsByKey({key}))
+  //   })
+  //
+  //     // this.store$.dispatch(GetLocationByKey({key: this.state.keys[0]}))
+  //     // this.store$.dispatch(GetConditionsByKey({key: this.state.keys[0]}))
+  //
+  //   // this.store$.dispatch(GetConditionsByKey({key: this.state.keys[0]}))
+  // }
 
   // onClickBtn() {
   //   // this.store$.dispatch(WeatherAddLocation())
   //   this.commonService.updateStoreWithGeneralLocations(['1', '2', '3'])
   // }
 
-  getLocationFromLink(link: string) {
-    let key = link.split('/').find(item => this.state.keys.includes(item));
-    return this.generalLocations[key.toString()]
-  }
+  // getLocationFromLink(link: string) {
+  //   let key = link.split('/').find(item => this.state.keys.includes(item));
+  //   return this.generalLocations[key.toString()]
+  // }
 
 }
